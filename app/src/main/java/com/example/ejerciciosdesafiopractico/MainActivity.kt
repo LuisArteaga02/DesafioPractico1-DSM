adfb1377550fc8bb7104297aa80463ee995c6c58
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2026-08-12 22:26:30 -0600

    add - Archivos Base de Activity y Layaout sin logica aun

--- app/src/main/java/com/example/ejerciciosdesafiopractico/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciosdesafiopractico/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.ejerciciosdesafiopractico
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnJ1 : Button
+    private lateinit var btnJ2 : Button
+    private lateinit var btnJ3 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        btnJ1 = findViewById<Button>(R.id.btnJ1)
+        btnJ2 = findViewById<Button>(R.id.btnJ2)
+        btnJ3 = findViewById<Button>(R.id.btnJ3)
+
+        btnJ1.setOnClickListener {
+            val intent = Intent(this, PromedioActivity::class.java)
         }
+        btnJ2.setOnClickListener {
+            val intent = Intent(this, SalarioActivity::class.java)
+        }
+        btnJ3.setOnClickListener {
+            val intent = Intent(this, CalculadoraActivity::class.java)
+        }
+
+
+
+
+
     }
 }
