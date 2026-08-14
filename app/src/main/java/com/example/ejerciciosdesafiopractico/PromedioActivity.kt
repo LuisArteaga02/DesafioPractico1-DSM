4ceee5540a9b9cacec73afa47f0258108b50f194
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2026-08-13 21:54:34 -0600

    improve - Diseño de XML de activity_main y promedio_activity

--- app/src/main/java/com/example/ejerciciosdesafiopractico/PromedioActivity.kt
+++ app/src/main/java/com/example/ejerciciosdesafiopractico/PromedioActivity.kt
@@ -1,5 +1,51 @@
 package com.example.ejerciciosdesafiopractico
 
-class PromedioActivity {
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ejerciciosdesafiopractico.ui.theme.EjerciciosDesafioPracticoTheme
+import org.w3c.dom.Text
+import android.widget.TextView
 
+class PromedioActivity : ComponentActivity() {
+
+    private val model = PromedioModel()
+    private val CHANNEL_ID = "canal_promedio_estudiantes"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.promedio_activity)
+
+        crearCanalNotificaciones()
+
+        val btnVolver = findViewById<Button>(R.id.btnVolver)
+        val etNombre = findViewById<EditText>(R.id.etNombre)
+        val etNotas = listOf(
+            findViewById<EditText>(R.id.etNota1),
+            findViewById<EditText>(R.id.etNota2),
+            findViewById<EditText>(R.id.etNota3),
+            findViewById<EditText>(R.id.etNota4),
+            findViewById<EditText>(R.id.etNota5),
+        )
+        val btnResultado = findViewById<Button>(R.id.btnResultado)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        btnVolver.setOnClickListener {
+            finish()
+        }
+        
+
+
+    }
 }
--- app/src/main/java/com/example/ejerciciosdesafiopractico/PromedioModel.kt
+++ app/src/main/java/com/example/ejerciciosdesafiopractico/PromedioModel.kt
@@ -1,4 +1,27 @@
 package com.example.ejerciciosdesafiopractico
 
+import java.text.DecimalFormat
+
 class PromedioModel {
+    private val ponderaciones = listOf(0.10, 0.15, 0.20,0.25, 0.30)
+
+    fun notaValida(nota: Double?): Boolean{
+        return nota != null && nota in 0.0..10.0
+    }
+    fun calcularPromedioPonderado(notas:List<Double>): Double{
+        var acumulado = 0.0
+        for (i in notas.indices){
+        acumulado += notas[i] * ponderaciones[i]
+        }
+        return acumulado
+    }
+    fun esAprobado(promedio: Double): Boolean{
+        return promedio >= 6.0
+    }
+
+    fun formatoDecimal(numero: Double): String{
+        val formato = DecimalFormat("0.00")
+        return formato.format(numero)
+    }
+
 }
