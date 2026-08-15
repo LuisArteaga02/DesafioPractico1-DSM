174c361cc765fb3b587856912a60b871e7d32880
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2026-08-14 23:43:59 -0600

    Add - Logica de Calculadora model

--- app/src/main/java/com/example/ejerciciosdesafiopractico/CalculadoraActivity.kt
+++ app/src/main/java/com/example/ejerciciosdesafiopractico/CalculadoraActivity.kt
@@ -1,5 +1,13 @@
 package com.example.ejerciciosdesafiopractico
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import androidx.activity.ComponentActivity
 
+class CalculadoraActivity : ComponentActivity() {
 
-class CalculadoraActivity {
 }
--- app/src/main/java/com/example/ejerciciosdesafiopractico/CalculadoraModel.kt
+++ app/src/main/java/com/example/ejerciciosdesafiopractico/CalculadoraModel.kt
@@ -1,4 +1,35 @@
 package com.example.ejerciciosdesafiopractico
 
+import java.text.DecimalFormat
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class CalculadoraModel {
+
+    fun sumar(a: Double, b: Double): Double = a + b
+
+    fun restar(a: Double, b: Double): Double = a - b
+
+    fun multiplicar(a: Double, b: Double): Double = a * b
+
+    fun dividir(a: Double, b: Double): Double? {
+        if (b == 0.0) return null
+        return a / b
+    }
+
+    fun exponente(base: Double, exp: Double): Double {
+        return base.pow(exp)
+    }
+
+    fun raizCuadrada(a: Double): Double? {
+        if (a < 0.0) return null 
+        return sqrt(a)
+    }
+
+
+    fun formatearResultado(valor: Double): String {
+        val df = DecimalFormat("#.####")
+        return df.format(valor)
+    }
+
 }
